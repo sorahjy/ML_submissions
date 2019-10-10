4e9bb0f3a595b54b95dd67aab03868eeaecbbf11
sorahjy <sorahjy@gmail.com>
2019-10-10 20:49:08 +0800

    'linear_regression'

--- regression.py
+++ regression.py
@@ -1,9 +1,11 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression, LinearRegression, Lasso, Ridge
+from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor, ExtraTreesRegressor
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
@@ -11,22 +13,44 @@ from sklearn.tree import DecisionTreeRegressor
 
 column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
 bos = pd.read_csv('datasets/data/housing_boston.csv', header=None, delimiter=r"\s+", names=column_names)
+random_state = 12
 
 
-def model_selection():
+def model_selection_linear():
     x, y = feature_selection()
     print(x.head())
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=7)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=random_state)
     pipelines = []
     pipelines.append(('LR', Pipeline([('Scaler', StandardScaler()), ('LR', LinearRegression())])))
-    pipelines.append(('Ridge', Pipeline([('Scaler', StandardScaler()), ('Ridge', Lasso())])))
+    pipelines.append(('Ridge', Pipeline([('Scaler', StandardScaler()), ('Ridge', Ridge())])))
     pipelines.append(('LASSO', Pipeline([('Scaler', StandardScaler()), ('LASSO', Lasso())])))
     pipelines.append(('CART', Pipeline([('Scaler', StandardScaler()), ('CART', DecisionTreeRegressor())])))
     pipelines.append(('SVR', Pipeline([('Scaler', StandardScaler()), ('SVR', SVR())])))
     results = []
     names = []
     for name, model in pipelines:
-        kfold = KFold(n_splits=10, random_state=5)
+        kfold = KFold(n_splits=10, random_state=random_state)
+        cv_results = cross_val_score(model, x_train, y_train, cv=kfold, scoring='neg_mean_squared_error')
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
+
+
+def model_selection_ensemble():
+    ensembles = []
+    x, y = feature_selection()
+    print(x.head())
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=random_state)
+    ensembles.append(('KNN', Pipeline([('Scaler', StandardScaler()), ('KNN', KNeighborsRegressor())])))
+    ensembles.append(('AB', Pipeline([('Scaler', StandardScaler()), ('AB', AdaBoostRegressor())])))
+    ensembles.append(('GBM', Pipeline([('Scaler', StandardScaler()), ('GBM', GradientBoostingRegressor())])))
+    ensembles.append(('RF', Pipeline([('Scaler', StandardScaler()), ('RF', RandomForestRegressor())])))
+    ensembles.append(('ET', Pipeline([('Scaler', StandardScaler()), ('ET', ExtraTreesRegressor())])))
+    results = []
+    names = []
+    for name, model in ensembles:
+        kfold = KFold(n_splits=10, random_state=random_state)
         cv_results = cross_val_score(model, x_train, y_train, cv=kfold, scoring='neg_mean_squared_error')
         results.append(cv_results)
         names.append(name)
@@ -48,10 +72,10 @@ def plot_corr():
     plt.show()
 
 
-if __name__ == '__main__':
+def plot_linear_reg():
     x, y = feature_selection()
     print(x.head())
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=5)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=random_state)
     lm = Ridge()
     model = lm.fit(x_train, y_train)
     pred_y = lm.predict(x_test)
@@ -62,4 +86,10 @@ if __name__ == '__main__':
     plt.show()
     mse = mean_squared_error(y_test, pred_y)
     print(mse)
-    model_selection()
+
+
+if __name__ == '__main__':
+    plot_corr()
+    plot_linear_reg()
+    model_selection_linear()
+    model_selection_ensemble()
